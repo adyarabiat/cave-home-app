4757b33eae7ffdd7b507bbd65e1d19d4e187dd18
adyarabiat <ady.arabiat@gmail.com>
2021-03-01 23:18:57 +0400

    adding the header by using React-bootstrap

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import { Container } from "react-bootstrap";
+
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Cave Home</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default App;
